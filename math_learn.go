a6531cb3826b77513781002d6eaaab777f62e177
Henry Goodwin <HGoodwin617@gmail.com>
2021-02-04 04:54:56 +0000

    initial testing

---
+++ math_learn.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// fmt.Printf("%.3f", math.Log(math.E))
+	var x, y float64
+	x = 5000000000000
+	y = (math.Log(x) / 2)
+	fmt.Printf("%E\n", y)
+}
