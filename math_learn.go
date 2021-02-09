d8cb2a5e130ac6c1d7fa2641749610ecdb4482c9
Henry Goodwin <HGoodwin617@gmail.com>
2021-02-09 17:33:37 -0500

    additional fixes

--- math_learn.go
+++ math_learn.go
@@ -9,6 +9,9 @@ func main() {
 	// fmt.Printf("%.3f", math.Log(math.E))
 	var x, y float64
 	x = 5000000000000
-	y = (math.Log(x) / 2)
-	fmt.Printf("%E\n", y)
+	y = math.Log(x) / 2
+	fmt.Printf(
+		"%E\n",
+		y,
+	)
 }
